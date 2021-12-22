b5755907c28231866ba4f671b9b274ca9b8077ef
felipecontroll <felipe@controllinformatica.com.br>
2021-12-22 11:41:39 -0300

    Aula JavaScript - desafio contador e to-do list

--- aula_javascript/assets/js/script.js
+++ aula_javascript/assets/js/script.js
@@ -1,1 +1,69 @@
-console.log('hello wolrd');
+// inicio script contador 
+
+let count = 0;
+var input = document.querySelector('[name="adicionar"]');
+
+const CURRENT_NUMBER = document.getElementById('currentNumber');
+
+function increment() {
+	count++;
+	CURRENT_NUMBER.innerHTML = count;
+
+    if (count >= 0) {
+       document.getElementById('currentNumber').style.color = 'black';
+    }
+    if (count > 5){
+        input.setAttribute('disabled', true);
+    } 
+   }
+
+function decrement() {
+    count--;
+    CURRENT_NUMBER.innerHTML = count;
+    if (count < 0) {
+        document.getElementById('currentNumber').style.color = 'red';
+    } else {
+        document.getElementById('currentNumber').style.color = 'black';
+    }
+    if (count < 6) {
+        input.removeAttribute('disabled');
+    }
+}
+
+function test() {
+	kdowkdpo;
+}
+// fim script contador 
+
+// inicio script da lista 
+const form = document.getElementById('task-form');
+const taskList = document.getElementById('tasks');
+
+form.onsubmit = function (e) {
+	e.preventDefault();
+	const inputField = document.getElementById('task-input');
+	addTask(inputField.value);
+	form.reset();
+};
+
+function addTask(description) {
+	const taskContainer = document.createElement('div');
+	const newTask = document.createElement('input');
+	const taskLabel = document.createElement('label');
+	const taskDescriptionNode = document.createTextNode(description);
+
+	newTask.setAttribute('type', 'checkbox');
+	newTask.setAttribute('name', description);
+	newTask.setAttribute('id', description);
+
+	taskLabel.setAttribute('for', description);
+	taskLabel.appendChild(taskDescriptionNode);
+
+	taskContainer.classList.add('task-item');
+	taskContainer.appendChild(newTask);
+	taskContainer.appendChild(taskLabel);
+
+	taskList.appendChild(taskContainer);
+}
+
+// fim script da lista 
